94e9bd117c88aea93a56ada46e2a22268811289a
chanceDira <chancedesire60@gmail.com>
2024-01-15 13:46:28 +0200

    feat: added pagination

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar/Navbar";
 import MovieCard from "../components/MovieCard/MovieCard";
 import { useDispatch } from "react-redux";
@@ -15,12 +15,16 @@ interface Movie {
   image: string;
 }
 
+const ITEMS_PER_PAGE = 4;
+
 const Home = () => {
   const dispatch = useDispatch();
   const router = useRouter();
   const movies = useSelector((state: RootState) => state.movies.data);
   const loading = useSelector((state: RootState) => state.movies.loading);
   const error = useSelector((state: RootState) => state.movies.error);
+
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     dispatch(fetchMovies() as any);
@@ -35,11 +39,21 @@ const Home = () => {
     }, 4000)
   }, [loading, error, movies, router])
 
+
+  const totalPages = Math.ceil(movies.length / ITEMS_PER_PAGE);
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const currentMovies = movies.slice(startIndex, endIndex);
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
   return (
     <div className="relative w-11/12 md:w-9/12 h-full">
       <Navbar />
 
-      <div className="mt-28 w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center mb-40 md:mb-4">
+      <div className="mt-28 w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center ">
         {loading ? (
           <div className=" text-white flex justify-center items-center">
             Loading
@@ -48,10 +62,26 @@ const Home = () => {
           ""
         )}
 
-        {movies.map((movie: Movie) => (
-          <MovieCard movie={movie} />
+{currentMovies.map((movie: Movie) => (
+          <MovieCard movie={movie} key={movie._id} />
         ))}
       </div>
+      <div className="flex justify-center mt-4">
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (page) => (
+            <button
+              key={page}
+              className={`mx-2 px-4 py-2 bg-primary_v2 text-white rounded-md mb-40 md:mb-4 ${
+                page === currentPage ? "bg-opacity-80" : "bg-opacity-40"
+              }`}
+              onClick={() => handlePageChange(page)}
+            >
+              {page}
+            </button>
+          )
+        )}
+      </div>
+
     </div>
   );
 };
